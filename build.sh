beaf276e4f7fb66fcd5d96dd88e4459c0f79e9f7
Brock Wilcox <awwaiid@thelackthereof.org>
2016-08-04 22:18:47 -0400

    Add basic api and some hashes

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ if [[ ! -e ~/.rakudobrew ]] ; then
   git clone https://github.com/tadzik/rakudobrew ~/.rakudobrew
   export PATH=~/.rakudobrew/bin:$PATH
   rakudobrew init
-  rakudobrew build moar 2017.07.1
+  rakudobrew build moar 2016.07.1
   rakudobrew build panda
   panda install Task::Star
 fi
