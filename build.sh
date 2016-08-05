0027a95ce5e9f65ca2a86250aebcda296351d4bc
Brock Wilcox <awwaiid@thelackthereof.org>
2016-08-05 11:27:17 -0400

    Change to assume system perl

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 
+sudo apt-get install libperl-dev libimager-perl
+
+# if [[ $(perl -V | grep shrplib) ]] ; then
+#   echo 'Perl with -Duseshrplib found!'
+# else
+#   echo 'Installing via perlbrew'
+#   perlbrew install perl-stable -Duseshrplib
+#   cpanm Imager
+# fi
+
 # Install rakudobrew if needed
 if [[ -e ~/.rakudobrew ]] ; then
   echo "Rakudobrew found!"
@@ -10,17 +20,9 @@ else
   echo 'eval "$(~/.rakudobrew/bin/rakudobrew init -)"' >> ~/.profile
   rakudobrew build moar 2016.07.1
   rakudobrew build panda
+  panda install Task::Star
+  panda install LREP
+  panda install Inline::Perl5
 fi
 
-if [[ $(perl -V | grep shrplib) ]] ; then
-  echo 'Perl with -Duseshrplib found!'
-else
-  echo 'Installing via perlbrew'
-  perlbrew install perl-stable -Duseshrplib
-fi
-
-panda --force install Task::Star
-panda --force install LREP
-panda --force install Inline::Perl5
-cpanm Imager
 
