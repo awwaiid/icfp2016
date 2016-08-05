88c076aee4ee943a5f46504487c7d6347a079e4c
Brock Wilcox <awwaiid@thelackthereof.org>
2016-08-05 00:28:51 -0400

    Not a bad monkey

--- lib/Oragami.pm
+++ lib/Oragami.pm
@@ -1,17 +1,18 @@
 class Oragami {
-
   method trivial_solution {
-      return "4
-0,0
-0,1
-1,1
-1,0
-1
-4 0 1 2 3
-0,0
-0,1
-1,1
-1,0";
+    return q:to/END/;
+      4
+      0,0
+      0,1
+      1,1
+      1,0
+      1
+      4 0 1 2 3
+      0,0
+      0,1
+      1,1
+      1,0
+      END
   }
 }
 
