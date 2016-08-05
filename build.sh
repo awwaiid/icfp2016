4263cf024a5eaa99d0e7188d35a32a38650a4405
Brock Wilcox <awwaiid@thelackthereof.org>
2016-08-04 21:37:27 -0400

    Add build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+
+# Install rakudobrew if needed
+if [[ ! -e ~/.rakudobrew ]] ; then
+  echo "Rakudobrew not found, installing"
+  git clone https://github.com/tadzik/rakudobrew ~/.rakudobrew
+  export PATH=~/.rakudobrew/bin:$PATH
+  rakudobrew init
+  rakudobrew build moar 2017.07.1
+  rakudobrew build panda
+  panda install Task::Star
+fi
+
+
