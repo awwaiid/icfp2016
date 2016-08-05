543bfac7d7f260cb51cf7bb5db47d7d90e20cf91
Brock Wilcox <awwaiid@thelackthereof.org>
2016-08-05 04:35:26 -0400

    Mess with build script

--- build.sh
+++ build.sh
@@ -1,18 +1,26 @@
 
 # Install rakudobrew if needed
-if [[ ! -e ~/.rakudobrew ]] ; then
+if [[ -e ~/.rakudobrew ]] ; then
+  echo "Rakudobrew found!"
+else
   echo "Rakudobrew not found, installing"
   git clone https://github.com/tadzik/rakudobrew ~/.rakudobrew
   export PATH=~/.rakudobrew/bin:$PATH
-  rakudobrew init
+  # rakudobrew init
+  echo 'eval "$(~/.rakudobrew/bin/rakudobrew init -)"' >> ~/.profile
   rakudobrew build moar 2016.07.1
   rakudobrew build panda
-  panda install Task::Star
-  panda install LREP
 fi
 
-if [[ ! $(perl -V | grep shrplib) ]] ; then
+if [[ $(perl -V | grep shrplib) ]] ; then
+  echo 'Perl with -Duseshrplib found!'
+else
+  echo 'Installing via perlbrew'
   perlbrew install perl-stable -Duseshrplib
-  panda install Inline::Perl5
 fi
+
+panda --force install Task::Star
+panda --force install LREP
+panda --force install Inline::Perl5
+cpanm Imager
 
