45ddb8230233ca871920fffb46f4897435400f22
lungching <mburns.lungching@gmail.com>
2016-08-06 15:38:16 -0400

    add function to submit solution for shifted, unfolded paper

--- lib/ICFP.pm
+++ lib/ICFP.pm
@@ -43,3 +43,27 @@ sub send-solution($id, $solution) is export {
   say qqx«curl -s --compressed -L -H Expect: -H 'X-API-Key: 31-99e01d42cda065257f188de236e944ef' -F 'problem_id=$id' -F 'solution_spec=@solution.txt' 'http://2016sv.icfpcontest.org/api/solution/submit'»;
   sleep 1;
 }
+
+sub shifted-solution( $problem ) is export  {
+    my $p = Problem::Grammar.parse-problem( $problem );
+    my ( $xmin, $ymin ) = $p.silhouette.get-min-coords();
+    my @square = (
+        [$xmin, $ymin],
+        [$xmin, $ymin + 1],
+        [$xmin + 1, $ymin + 1],
+        [$xmin + 1, $ymin],
+    );
+
+    my $solution = "4\n";
+    my $point_list = '';
+    for @square -> ($x,$y) {
+        my $x_str = $x.denominator == 1 ?? $x !! $x.numerator ~ '/' ~ $x.denominator;
+        my $y_str = $y.denominator == 1 ?? $y !! $y.numerator ~ '/' ~ $y.denominator;
+        $point_list ~= "$x_str,$y_str\n";
+    }
+    $solution ~= "0,0\n0,1\n1,1\n1,0\n";
+    $solution ~= "1\n4 0 1 2 3\n";
+    $solution ~= $point_list;
+
+    return $solution;
+}
